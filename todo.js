e4df18d6e873ab7020296d20eac2269edcf59063
Olivia DeMoss <odemoss@goodwillworks.org>
2021-10-18 19:31:32 -0400

    hello

--- todo.js
+++ todo.js
@@ -1,9 +1,16 @@
 const lists = [];
 
-    function todoList(){
-        let textInput = document.getElementById("inputTodo").value;
-        let createText = document.createTextNode(textInput);
-        let createItem = document.createElement("li");
-        createItem.appendChild(createText);
-        document.getElementById("todoList").appendChild(createItem); 
+function todoList(){
+    for ( let i = 0; i < locallist.length; i++ ) {
+        let list = localList[i];
+        let listObj = new List(localList[i].todoList)
+
+        for ( let j = 0; j < list.items.length; j++ ) {
+			// inside our items in the array
+			// call our addItem function 
+			listObj.addItem(list.items[j].value, list.items[j].timeStamp, list.items[j].completed );
+		}
+
+		lists.push( listObj );
+	}
 }
